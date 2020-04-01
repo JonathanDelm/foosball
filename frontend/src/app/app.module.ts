90e6c04ed0326d01738fb6e9c47a79fabb2ec80f
JonathanDelm <j.delmeiren@gmail.com>
2020-04-01 10:48:32 +0200

    Added start of navigation with routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,17 +7,32 @@ import { MatchHistoryComponent } from './match-history/match-history.component';
 import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 import { MaterialModule } from './material/material.module';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'new', component: AddMatchComponent },
+  { path: 'history', component: MatchHistoryComponent },
+  { path: 'scoreboard', component: ScoreboardComponent },
+  { path: '', redirectTo: 'new', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AddMatchComponent,
     MatchHistoryComponent,
-    ScoreboardComponent
+    ScoreboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
